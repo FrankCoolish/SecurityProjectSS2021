2305331277029bb55e1cf8453714b67e4d5deda0
fcoolish <frank.kulisch@posteo.de>
2021-05-29 10:56:57 +0200

    funktionierendes php beispiel

--- Sewing/showreviews.php
+++ Sewing/showreviews.php
@@ -1,39 +1,14 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <body>
-        <?php
-        $hostname = "localhost:3036";
-        $username = "review_site";
-        $password = "JxSLRkdutW";
-        $db = "reviews";
-
-        $dbconnect=  mysqli_connect($hostname,$username,$password,$db);
-
-        if ($dbconnect->connect_error) {
-            die("Database connection failed: " . $dbconnect->connect_error);
+    <?php
+        $user = "review_site";
+        $pass = "JxSLRkdutW";
+        $pdo = new PDO('mysql:host=localhost;dbname=reviews', $user, $pass );
+        $sql = "SELECT * FROM user_review";
+        foreach($pdo->query($sql) as $row) {
+            echo $row['reviewer_name']."<br />";
         }
         ?>
-
-        <table border="1" align="center">
-            <tr>
-                <td>Reviewer Name</td>
-                <td>Stars</td>
-                <td>Details</td>
-            </tr>
-        <?php
-
-        $query = mysqli_query($dbconnect, "SELECT * FROM user_review")
-        or die (mysqli_error($dbconnect));
-
-        while ($row = mysqli_fetch_array($query)) {
-            echo
-            "<tr>
-            <td>{$row['reviewer_name']}</td>
-            <td>{$row['star_rating']}</td>
-            <td>{$row['details']}</td>
-           </tr>\n";
-        }
-        ?>
-        </table>
     </body>
 </html>
